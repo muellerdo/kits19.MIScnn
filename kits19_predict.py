03231908a28624829ce119119d182cc703d3b25b
muellerdo <domey038@live.de>
2019-07-29 13:38:39 +0200

    Add model, predictions and scripts

--- kits19_predict.py
+++ kits19_predict.py
@@ -8,9 +8,11 @@
 import sys
 import argparse
 import os
+import numpy as np
 # Internal libraries/scripts
 import MIScnn.neural_network as MIScnn_NN
 import MIScnn.evaluation as MIScnn_CV
+from MIScnn.data_io import save_prediction
 
 #-----------------------------------------------------#
 #                  Parse command line                 #
@@ -50,38 +52,46 @@ args = parser.parse_args()
 #-----------------------------------------------------#
 config = dict()
 # Dataset
-config["cases"] = list(range(2,5))
+config["cases"] = list(range(210,300))
 config["data_path"] = args.args_input           # Path to the kits19 data dir
 config["model_path"] = "model"                  # Path to the model data dir
 config["output_path"] = "predictions"           # Path to the predictions directory
 config["evaluation_path"] = "evaluation"        # Path to the evaluation directory
 # GPU Architecture
-config["gpu_number"] = 1                        # Number of GPUs (if > 2 = multi GPU)
+config["gpu_number"] = 2                        # Number of GPUs (if > 2 = multi GPU)
 # Neural Network Architecture
-config["input_shape"] = (None, 16, 16, 1)       # Neural Network input shape
-config["patch_size"] = (16, 16, 16)             # Patch shape/size
+config["input_shape"] = (None, 128, 128, 1)     # Neural Network input shape
+config["patch_size"] = (48, 128, 128)           # Patch shape/size
 config["classes"] = 3                           # Number of output classes
-config["batch_size"] = 16                       # Number of patches in on step
+config["batch_size"] = 10                       # Number of patches in on step
 # Training
-config["epochs"] = 5                            # Number of epochs for training
+config["epochs"] = 20                           # Number of epochs for training
 config["max_queue_size"] = 3                    # Number of preprocessed batches
 config["learninig_rate"] = 0.0001               # Learninig rate for the training
 config["shuffle"] = True                        # Shuffle batches for training
 # Data Augmentation
-config["overlap"] = (0,0,0)                     # Overlap in (x,y,z)-axis
+config["overlap"] = (12, 32, 32)                # Overlap in (x,y,z)-axis
 config["skip_blanks"] = True                    # Skip patches with only background
 config["scale_input_values"] = False            # Scale volume values to [0,1]
-config["rotation"] = False                      # Rotate patches in 90/180/270°
-config["flipping"] = False                      # Reflect/Flip patches
+config["rotation"] = True                       # Rotate patches in 90/180/270°
+config["flipping"] = True                       # Reflect/Flip patches
 config["flip_axis"] = (3)                       # Define the flipping axes (x,y,z <-> 1,2,3)
 # Prediction
-config["pred_overlap"] = False                  # Usage of overlapping patches in prediction
+config["pred_overlap"] = True                   # Usage of overlapping patches in prediction
 # Evaluation
 config["n_folds"] = 3                           # Number of folds for cross-validation
-config["per_split"] = 0.20                      # Percentage of Testing Set for split-validation
-config["n_loo"] = 1                             # Number of cycles for leave-one-out
+config["per_split"] = 0.095                     # Percentage of Testing Set for split-validation
+config["n_loo"] = 3                             # Number of cycles for leave-one-out
 config["visualize"] = True                      # Print out slice images for visual evaluation
 config["class_freq"] = False                    # Calculate the class frequencies for each slice
+
+
+#-----------------------------------------------------#
+#          GPU Management for shared hardware         #
+#-----------------------------------------------------#
+import tensorflow as tf
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.65)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 #-----------------------------------------------------#
 #                    Runner code                      #
@@ -89,21 +99,17 @@ config["class_freq"] = False                    # Calculate the class frequencie
 # Output the configurations
 print(config)
 
-# Create the Convolutional Neural Network
-#cnn_model = MIScnn_NN.NeuralNetwork(config)
+# Create the 3D Residual U-Net
+cnn_model = MIScnn_NN.NeuralNetwork(config)
 
-# Train the Convolutional Neural Network model
-#cnn_model.train(config["cases"])
-# Dump the model
-#cnn_model.dump("model")
+# Load the already fitted model from file
+cnn_model.load("residual_unet")
 
-# Load a model
-#cnn_model.load("model")
+# Predict kits19 test set with the fitted 3D Residual U-Net model
+cnn_model.predict(config["cases"])
 
-# Predict a segmentation with the Convolutional Neural Network model
-#cnn_model.predict(list(range(3,4)))
-
-# Evaluate the Convolutional Neural Network
-#MIScnn_CV.cross_validation(config)
-#MIScnn_CV.leave_one_out(config)
-MIScnn_CV.split_validation(config)
+# Resize enlarged 268 ct sample to original size back
+pred = load_prediction_nii(268, config["output_path"]).get_data()
+vol = load_volume_nii(268, config["data_path"]).get_data()
+pred = np.resize(pred, vol.shape)
+save_prediction(pred, 268, config["output_path"])
